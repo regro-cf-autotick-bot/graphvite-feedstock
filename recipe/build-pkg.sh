1c8e62c7f605e709061a640c8e1227ec38e402f2
H. Vetinari <h.vetinari@gmx.com>
2024-08-09 18:05:28 +1100

    switch to ninja

--- recipe/build-pkg.sh
+++ recipe/build-pkg.sh
@@ -14,15 +14,15 @@ elif [[ ${cuda_compiler_version} == 12.0 ]]; then
     export CMAKE_CUDA_ARCHS="53-real;62-real;72-real;75-real;80-real;86-real;89-real;90"
 fi
 
-cmake \
+cmake -G Ninja \
     ${CMAKE_ARGS} \
     -DPROJECT_BINARY_DIR=$PREFIX \
     -DFAISS_PATH=$PREFIX/lib/libfaiss.so \
     -DCMAKE_CUDA_ARCHITECTURES="${CMAKE_CUDA_ARCHS}" \
     ..
 
-make
-make install
+cmake --build .
+cmake --install .
 
 cd ../python
 $PYTHON setup.py install
